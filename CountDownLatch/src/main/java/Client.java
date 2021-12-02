19662ba407760ab2896ff579c1925b7d65128966
HasanSajjadSwati <swati.hasansajjad@gmail.com>
2021-12-02 15:44:16 +0500

    Fixed waitingMessage

--- CountDownLatch/src/main/java/Client.java
+++ CountDownLatch/src/main/java/Client.java
@@ -5,32 +5,45 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 
 public class Client {
     
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Socket socket = new Socket("127.0.0.1",8081);
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         System.out.println("Send Request To Server:");
         Scanner scanner = new Scanner(System.in);
         String inputToServer = null;
-        
+        String outputFromServer = null;
+            
+   
+
         while(!"exit".equalsIgnoreCase(inputToServer)){
             inputToServer = scanner.nextLine();
-            waitingMessage message = new waitingMessage();
             out.println(inputToServer);
+            
+            CountDownLatch latch = new CountDownLatch(1);
+            waitingMessage message = new waitingMessage(latch);
             Thread thread = new Thread(message);
             thread.start();
+            outputFromServer = in.readLine();
             
-            if(inputToServer.equalsIgnoreCase("exit"))
-                System.out.println(in.readLine());
             
-            else
-                System.out.println(in.readLine() + " Waited = " + message.getCounter() + "Seconds!");
-            message.resetCounter();
-            thread.stop();
+            if(outputFromServer.equalsIgnoreCase("response received!")){
+                System.out.println("Response Received! Waited = " +message.getCounter() + " Seconds!" );
+                message.setExit(true);
+                message.resetCounter();
+            }
+            
+            if(outputFromServer.equalsIgnoreCase("Goodbye!"))
+            {
+                System.out.println("Goodbye!");
+                message.setExit(true);
+            }
         }
         scanner.close();
     }
@@ -40,16 +53,25 @@ public class Client {
 
 class waitingMessage implements Runnable {
     private static int counter = 0;
+    private final CountDownLatch latch;
+    private boolean exitStatus = false;
+    
+    public waitingMessage(CountDownLatch latch){
+        this.latch = latch;
+    }
+    
     @Override
     public void run() {
-        while(true){
+        
+        while(!exitStatus){
             try {
-                Thread.sleep(1000);
-                counter++;
-                System.out.println("I am waiting for response from server!");
-            } catch (InterruptedException ex) {
-                ex.getStackTrace();
-            }
+            
+            System.out.println("I am waiting for response from server!");
+            counter++;
+            latch.await(1,TimeUnit.SECONDS);
+        } catch (InterruptedException ex) {
+            ex.getStackTrace();
+        }
         }
     }
     
@@ -58,5 +80,8 @@ class waitingMessage implements Runnable {
     }
     public void resetCounter(){
         counter = 0;
+    }
+    public void setExit(Boolean exitStatus){
+        this.exitStatus = exitStatus;
     }
 }
